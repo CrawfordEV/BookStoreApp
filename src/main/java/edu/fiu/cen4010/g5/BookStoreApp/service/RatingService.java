8b2b3a1229c3855cf9248995af527e880f5df4ab
Eric Crawford <ecraw007@fiu.edu>
2021-10-23 15:27:21 -0400

    Implemented rating value validations for POST/PUT, and created functionality/mapping to sort ratings for a book in ascending order as well as descending.

--- src/main/java/edu/fiu/cen4010/g5/BookStoreApp/service/RatingService.java
+++ src/main/java/edu/fiu/cen4010/g5/BookStoreApp/service/RatingService.java
@@ -1,6 +1,7 @@
 package edu.fiu.cen4010.g5.BookStoreApp.service;
 
 import edu.fiu.cen4010.g5.BookStoreApp.model.Rating;
+import edu.fiu.cen4010.g5.BookStoreApp.model.RatingSortedByValueAsc;
 import edu.fiu.cen4010.g5.BookStoreApp.model.RatingSortedByValueDes;
 import edu.fiu.cen4010.g5.BookStoreApp.repository.RatingRepository;
 import org.springframework.stereotype.Service;
@@ -21,32 +22,41 @@ public class RatingService {
     public void addRating(Rating rating) {
 
         // TODO: Validate user and book from other features (dependencies)
-        // TODO: Validate value 0-5 stars
 
+        // validate the value passed for the rating (1-5 stars)
+        int v = rating.getValue();
+        if (v > 5 || v < 1) {
+            throw new RuntimeException(String.format("Invalid value. Ratings must be between 1-5."));
+        }
+
+        // get a list of all ratings in the database by this user
         Optional<List<Rating>> repositoryResults = ratingRepository.findByUserId(rating.getUserid());
 
-        // if no ratings already exist for this user, go ahead and add new rating (user/book already validated)
+        // if no ratings already exist for this user, go ahead and add the new rating (user/book were validated earlier)
         if (repositoryResults.isPresent() == false) {
             ratingRepository.insert(rating);
         }
 
-        // if ratings do exist by this user, check that there is not already a rating for this book
+        // some ratings do exist by this user
         else {
+
+            // check that there is not already an existing rating by this user for the specified book
             List<Rating> queryResultsForUser = repositoryResults.get();
             List<Rating> queryResultsForUserAndBook = new ArrayList<>();
-
             for (Rating r : queryResultsForUser) {
                 if (r.getBookid().equals(rating.getBookid())) {
                     queryResultsForUserAndBook.add(r);
                 }
             }
 
+            // no rating by this user for this book, so insert it
             if (queryResultsForUserAndBook.size() == 0) {
-                // no rating by this user for this book, so insert it
                 ratingRepository.insert(rating);
             }
+
+            // rating for this book by this user already exists, so throw error
+            // PUT API should be used to update instead of insert
             else {
-                // rating for this book by this user already exists, use the Put API to update instead of insert
                 throw new RuntimeException(String.format("Found Existing Rating for Book ID %s by User ID %s", rating.getBookid(), rating.getUserid()));
             }
         }
@@ -55,46 +65,46 @@ public class RatingService {
     public void updateRating(Rating rating) {
 
 //        TODO: Validate user and book from other features (dependencies)
-//        TODO: Validate value 0-5 stars
 
-////      For testing.
-//        System.out.println("INSIDE OF UPDATE BEFORE CALL TO REPOSITORY FUNCTION");
+        // validate the value passed for the rating (1-5 stars)
+        int v = rating.getValue();
+        if (v > 5 || v < 1) {
+            throw new RuntimeException(String.format("Invalid value. Ratings must be between 1-5."));
+        }
 
+        // get a list of all ratings in the database by this user
         Optional<List<Rating>> repositoryResults = ratingRepository.findByUserId(rating.getUserid());
 
+        // if no ratings by this user exist, we cannot update one so throw an error
+        // POST API should be used to insert new ratings instead of update
         if (repositoryResults.isPresent() == false) {
             throw new RuntimeException(String.format("Cannot find Ratings by User %s", rating.getUserid()));
         }
+
+        // some ratings by this user exist, so find the one for the specified book if it exists
         else {
             List<Rating> queryResultsForUser = repositoryResults.get();
             List<Rating> queryResultsForUserAndBook = new ArrayList<>();
-
             for (Rating r : queryResultsForUser) {
-
-////              For testing purposes
-//                System.out.println(r.getUserid() + " : " + r.getBookid());
-
                 if (r.getBookid().equals(rating.getBookid())) {
                     queryResultsForUserAndBook.add(r);
                 }
             }
 
-////          For testing purposes
-//            System.out.println(queryResultsForUserAndBook.size());
-//            for (Rating r : queryResultsForUserAndBook) {
-//                System.out.println(r.getUserid() + " : " + r.getBookid());
-//
-//            }
-
+            // could not find a rating for the specified book by this user, so throw an error
+            // POST API should be used to insert new ratings instead of update
             if (queryResultsForUserAndBook.size() == 0) {
                 throw new RuntimeException(String.format("Cannot Find Rating for Book ID %s by User ID %s", rating.getBookid(), rating.getUserid()));
-            } else if (queryResultsForUserAndBook.size() > 1) {
-////              For testing purposes
-//                for (Rating r : queryResultsForUserAndBook) {
-//                    System.out.println(r.getUserid() + " : " + r.getBookid());
-//                }
+            }
+
+            // for some reason, multiple ratings for the specified book by this user were found, so throw an error
+            // only one rating should exist per user per book
+            else if (queryResultsForUserAndBook.size() > 1) {
                 throw new RuntimeException(String.format("Found Duplicate Ratings for Book ID %s by User ID %s", rating.getBookid(), rating.getUserid()));
-            } else {
+            }
+
+            // only one rating by this user for the specified book exists, so update it
+            else {
                 Rating savedRating = queryResultsForUserAndBook.get(0);
 
                 savedRating.setUserid(rating.getUserid());
@@ -111,9 +121,6 @@ public class RatingService {
 //                .orElseThrow(() -> new RuntimeException(
 //                        String.format("Cannot Find Rating for Book ID %s by User ID %s", rating.getBookid(), rating.getUserid())
 //                ));
-
-////      For testing
-//        System.out.println("REPOSITORY FUNCTION RAN");
 
     }
 
@@ -142,6 +149,15 @@ public class RatingService {
 
         List<Rating> unsortedRatings = getRatingsByBook(bookid);
         unsortedRatings.sort(new RatingSortedByValueDes());
+
+        return unsortedRatings;
+    }
+
+    public List<Rating> getRatingsByBookSortedAsc(String bookid) {
+        // validation of book and existence of results performed in getRatingsByBook method
+
+        List<Rating> unsortedRatings = getRatingsByBook(bookid);
+        unsortedRatings.sort(new RatingSortedByValueAsc());
 
         return unsortedRatings;
     }
